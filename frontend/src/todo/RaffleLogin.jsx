c6b695a0e43e4d4bb90165dc88dea3ac88362ab6
leandrocarra-luizalabs <leandrocarra@luizalabs.com>
2018-09-24 23:54:37 -0300

    add storagelogin

--- frontend/src/todo/RaffleLogin.jsx
+++ frontend/src/todo/RaffleLogin.jsx
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import IconLogin from './IconLogin';
 
 class RaffleLogin extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: '',
+      surName: ''
+    }
+  }
   render() {
     const handleSetLoginUser = (e) => {
-      let teste = e.target.value
-      console.log(teste)
+      let loginUserName = e.target.value;
+      this.setState({userName: loginUserName})
     }
+    const handleSetLoginSurName = (e) => {
+      let loginSurName = e.target.value;
+    }
+
+    const handleSetStorageLogin = () => {
+      console.log(this.state.userName)
+    }
+
     return (
       <span>
         <div className="raffle-login"></div>
@@ -34,7 +49,9 @@ class RaffleLogin extends Component {
             <input
               className="raffle-login__submit"
               type="submit"
-              value="&#8594;"/>
+              value="&#8594;"
+              onClick={handleSetStorageLogin}
+              />
           </div>
         </div>
       </span>
